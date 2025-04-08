c4436233f102f0ed85d40bcf103a65b1aaf62b5f
Aagrawal27 <aarav_agrawal27@milton.edu>
2025-04-08 08:57:04 -0400

    commit

--- src/MyFarm.java
+++ src/MyFarm.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.List;
 
 public class MyFarm {
 
@@ -6,7 +7,8 @@ public class MyFarm {
      * Goal of this project:
      * Practice with 1-dimensional arrays of objects
      */
-    Plot[] row;
+    Plot[] rowArr;
+    ArrayList<Plot> rowList;
 
     public static void main(String[] args) {
         MyFarm garden = new MyFarm();
@@ -14,9 +16,9 @@ public class MyFarm {
 
     public MyFarm() {
 
-        row = new Plot[10];
+        rowArr = new Plot[10];
         for (int z = 0; z < 10; z++) {
-            row[z] = new Plot();
+            rowArr[z] = new Plot();
         }
 
         /***
@@ -25,22 +27,35 @@ public class MyFarm {
          * e.g. for the totalPlants() method, your output should look something like:
          * "There are 730 total plants across all the plots"
          */
-
+        displayPlots();
+        printPlantNames();
+        totalPlants();
+        tomatoLocations();
+        totalCarrots();
+        averageNumberOfPlants();
+        numberOfCarrotPlots();
+        numberOfEmptyPlots();
+        everyOtherNeedsWater();
+        plantWithMaxNumber();
     }
 
     public void displayPlots() {
         // use the printPlotInfo() method in Plot.java to print all information for each plot in the row
-
+        for(int i = 0; i< rowArr.length; i++){
+            rowArr[i].printPlotInfo();
+        }
     }
 
     public void printPlantNames() {
         // print the name of each plant in the row and its index
-
+        for(int i = 0; i< rowArr.length; i++){
+            System.out.println(rowArr[i].plantName);
+        }
     }
 
     public void totalPlants() {
         // how many total plants are there in the row?
-
+        System.out.println(rowArr.length);
     }
 
     public void tomatoLocations() {
@@ -48,37 +63,97 @@ public class MyFarm {
         // call printPlantNames() method to make sure you're getting the correct indexes
 
         // Note: this one needs a for loop, NOT a for each loop
+        List<Integer> tomatoIndexes = new ArrayList<>();
 
+        for(int i = 0; i< rowArr.length; i++){
+            if(rowArr[i].plantName=="tomato"){
+                tomatoIndexes.add(i);
+            }
+        }
+        System.out.println(tomatoIndexes);
     }
 
     public void totalCarrots() {
         // how many total carrot plants are there?
+        int totalCarrots = 0;
 
+        for(int i = 0; i< rowArr.length; i++){
+            if(rowArr[i].plantName=="carrot"){
+                totalCarrots+=1;
+            }
+        }
+        System.out.println(totalCarrots);
     }
 
     public void averageNumberOfPlants() {
         // what is the average number of plants per plot?
+        int NumberPlantsPerPlot=0;
+        int averageNumberOfPlants;
 
+        for(int i = 0; i< rowArr.length; i++){
+            NumberPlantsPerPlot+= rowArr[i].numberOfPlants;
+        }
+
+        averageNumberOfPlants = NumberPlantsPerPlot/ rowArr.length+1;
+        System.out.println(averageNumberOfPlants);
     }
 
     public void numberOfCarrotPlots() {
         // how many plots have carrots on them?
-
+        int numCarrots = 0;
+        for(int i = 0; i< rowArr.length; i++) {
+            if (rowArr[i].plantName == "carrot") {
+                numCarrots++;
+            }
+        }
+        System.out.println(numCarrots);
     }
 
     public void numberOfEmptyPlots() {
         // how many plots are empty?
-
+        int numEmptyPlots = 0;
+        for(int i = 0; i< rowArr.length; i++) {
+            if (rowArr[i].plantName == "empty") {
+                numEmptyPlots++;
+            }
+        }
+        System.out.println(numEmptyPlots);
     }
 
     public void everyOtherNeedsWater(){
         // change the value of needsWater to be true for even numbered indexes and false for odd numbered indexes
-
+        for(int i = 0; i< rowArr.length; i++) {
+            if (i%2==0) {
+                rowArr[i].needsWater = true;
+            }else{
+                rowArr[i].needsWater = false;
+            }
+        }
     }
 
     public void plantWithMaxNumber() {
         // which plant type has the most total plants?
+        String plantWithMost = "";
+        int marginOfVictory = 0;
 
+        for(int i = 0; i< rowArr.length; i++) {
+            if(rowArr[i].plantName!= plantWithMost && marginOfVictory<1) {
+                if (rowArr[i].plantName == "carrot") {
+                    plantWithMost = rowArr[i].plantName;
+                } else if (rowArr[i].plantName == "tulip") {
+                    plantWithMost = rowArr[i].plantName;
+                } else if (rowArr[i].plantName == "lettuce") {
+                    plantWithMost = rowArr[i].plantName;
+                } else if (rowArr[i].plantName == "tomato") {
+                    plantWithMost = rowArr[i].plantName;
+                }
+            }else if(rowArr[i].plantName== plantWithMost){
+                marginOfVictory+=1;
+            }else if (rowArr[i].plantName!= plantWithMost){
+                marginOfVictory-=1;
+            }
+        }
+        System.out.println(plantWithMost);
     }
 
     public void plantWithMinNumber() {
